692b32fe931fcfba96368a94cee0e636257a8c23
sbowe1 <sidneyb1202@gmail.com>
2022-07-01 00:21:19 -0700

    all but gauge functional

--- plots.js
+++ plots.js
@@ -6,9 +6,10 @@ let data = d3.json('data/samples.json').then(data => {
         dropdown.append('option').text(name).property('value', name);
 })});
 
+
 // Horizontal bar chart with dropdown menu for top 10 OTUs found in that individual
 function bar(sampleID) {
-    let data = d3.json('data/samples.json').then(data => {
+    d3.json('data/samples.json').then(data => {
         let samples = data.samples;
         
         // Filter for specific sample ID
@@ -30,16 +31,15 @@ function bar(sampleID) {
             showlegend: false,
             xaxis: {title: 'Sample Value'},
             yaxis: {title: 'OTU ID'},
-            width: '100%',
             height: '100%'
         };
-    Plotly.newPlot('#bar', dataBar, layoutBar);
-    })
+    Plotly.newPlot('bar', dataBar, layoutBar);
+    });
 }
 
 // Demographics panel
 function demographics(id) {
-    let data = d3.json('data/samples.json').then(data => {
+    d3.json('data/samples.json').then(data => {
         let metadata = data.metadata;
         let panel = d3.select('#demographics');
         let filtered = metadata.filter(sampleName => sampleName.id == id)[0];
@@ -49,10 +49,9 @@ function demographics(id) {
     });
 }
 
-
 // Bubble chart
 function bubble(sampleID) {
-    let data = d3.json('data/samples.json').then(data => {
+    d3.json('data/samples.json').then(data => {
         let samples = data.samples;
 
         // Filter for specific sample ID
@@ -62,36 +61,92 @@ function bubble(sampleID) {
         let otuLabels = filterSample.otu_labels;
         
         let dataBubble = [{
-            x: otuIDs,
-            y: sampleValues,
-            text: otuLabels,
+            x: otuIDs.slice(0,10).map(otuID => otuID).reverse(),
+            y: sampleValues.slice(0, 10).reverse(),
+            text: otuLabels.slice(0, 10).reverse(),
             mode: 'markers',
             marker: {
-                size: sampleValues,
-                color: otuIDs
+                size: sampleValues.slice(0, 10).reverse(),
+                color: otuIDs.slice(0, 10).reverse()
             }
         }];
         let layoutBubble = {
             title: `ID ${sampleID}: OTUs by Value`,
             showlegend: false,
-            xaxis: {title: 'OTU ID'},
-            yaxis: {title: 'Sample Value'},
+            xaxis: {
+                title: 'OTU ID',
+                rangemode: 'tozero',
+                autorange: true
+            },
+            yaxis: {
+                title: 'Sample Value',
+                rangemode: 'tozero',
+                autorange: true
+            },
             width: '100%',
             height: '100%'
         };
-        Plotly.newPlot('#bubble', dataBubble, layoutBubble);
+        Plotly.newPlot('bubble', dataBubble, layoutBubble);
     });
+}
+
+// Gauge
+function gauge(sampleID) {
+    d3.json('data/samples.json').then(data => {
+        let metadata = data.metadata;
+
+        let filterMeta = metadata.filter(sampleName => sampleName.id == sampleID)[0];
+
+        let dataGauge = [{
+            domain: {x: [0, 1], y: [0, 1]},
+            value: filterMeta.wfreq,
+            gauge: {
+                steps: [
+                    {range: [0, 1], text: '0-1'},
+                    {range: [1, 2], text: '1-2'},
+                    {range: [2, 3], text: '2-3'},
+                    {range: [3, 4], text: '3-4'},
+                    {range: [4, 5], text: '4-5'},
+                    {range: [5, 6], text: '5-6'},
+                    {range: [6, 7], text: '6-7'},
+                    {range: [7, 8], text: '7-8'},
+                    {range: [8, 9], text: '8-9'}, 
+                ]
+            },
+            type: 'indicator',
+            mode: 'gauge'
+        }];
+        let layout = {
+            title: 'Scrubs per Week',
+            height: '100%'
+        };
+        Plotly.newPlot('gauge', dataGauge, layout);
+    })
 }
 
 // Initial
 function init(choice) {
     demographics(choice);
     bar(choice);
+    gauge(choice);
     bubble(choice);
 }
 
 // Set initial set of graphs as ID 940
 init(940);
 
+// Update function
+function update(choice) {
+    // Reset panel text so only one ID's info is showing at once
+    let panel = d3.select('#demographics');
+    panel.text(' ')
+
+    demographics(choice);
+    bar(choice);
+    gauge(choice);
+    bubble(choice);
+}
+
 // Update when changing dropdown ID
-dropdown.on('change', init);
+dropdown.on('change', update);
+
